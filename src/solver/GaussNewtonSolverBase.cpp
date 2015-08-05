aae85512b635d33fc0ab15a7173f81dd08c9c9e7
Sean Anderson <sean.anderson@mail.utoronto.ca>
2015-08-05 02:43:52 -0400

    use map to avoid dynamic resizes

--- src/solver/GaussNewtonSolverBase.cpp
+++ src/solver/GaussNewtonSolverBase.cpp
@@ -136,12 +136,13 @@ void GaussNewtonSolverBase::buildGaussNewtonTerms() {
   BlockSparseMatrix A_(sqSizes, true);
   BlockVector b_(sqSizes);
 
-  // Declare memory
-  Eigen::MatrixXd a_add; // make map<pair<int,int>, matrix>
+  // In order to avoid resizing dynamic matrices, we keep a map of matrices
+  // for the various different block sizes
+  typedef std::pair<unsigned int, unsigned int> MatrixKey;
+  std::map<MatrixKey, Eigen::MatrixXd> matrixMap;
 
   // For each cost term
-  //#pragma omp parallel for num_threads(NUMBER_OF_OPENMP_THREADS)
-  #pragma omp parallel for private(a_add) num_threads(NUMBER_OF_OPENMP_THREADS)
+  #pragma omp parallel for private(matrixMap) num_threads(NUMBER_OF_OPENMP_THREADS)
   for (unsigned int c = 0 ; c < this->getProblem().getCostTerms().size(); c++) {
 
     // Compute the weighted and whitened errors and jacobians
@@ -174,20 +175,23 @@ void GaussNewtonSolverBase::buildGaussNewtonTerms() {
         // Calculate terms needed to update the Gauss-Newton left-hand side
         unsigned int row;
         unsigned int col;
+        MatrixKey key;
         if (blkIdx1 <= blkIdx2) {
           row = blkIdx1;
           col = blkIdx2;
-          a_add = jacobians[i].jac.transpose()*jacobians[j].jac;
+          key = std::make_pair(jacobians[i].jac.cols(), jacobians[j].jac.cols());
+          matrixMap[key] = jacobians[i].jac.transpose()*jacobians[j].jac;
         } else {
           row = blkIdx2;
           col = blkIdx1;
-          a_add = jacobians[j].jac.transpose()*jacobians[i].jac;
+          key = std::make_pair(jacobians[j].jac.cols(), jacobians[i].jac.cols());
+          matrixMap[key] = jacobians[j].jac.transpose()*jacobians[i].jac;
         }
 
         // Update the left-hand side (thread critical)
         #pragma omp critical(a_update)
         {
-          A_.add(row, col, a_add);
+          A_.add(row, col, matrixMap[key]);
         }
       }
     }
