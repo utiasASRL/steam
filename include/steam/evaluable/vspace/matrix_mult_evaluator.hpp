53060505f4ac84315ee28829f664895d1da56ff9
Yuchen Wu <cheneyuwu@gmail.com>
2022-10-07 01:07:00 +0000

    more generalizable matrix vector multiplication

--- include/steam/evaluable/vspace/matrix_mult_evaluator.hpp
+++ include/steam/evaluable/vspace/matrix_mult_evaluator.hpp
@@ -7,15 +7,15 @@
 namespace steam {
 namespace vspace {
 
-template <int DIM = Eigen::Dynamic>
-class MatrixMultEvaluator : public Evaluable<Eigen::Matrix<double, DIM, 1>> {
+template <int ROW = Eigen::Dynamic, int COL = ROW>
+class MatrixMultEvaluator : public Evaluable<Eigen::Matrix<double, ROW, 1>> {
  public:
   using Ptr = std::shared_ptr<MatrixMultEvaluator>;
   using ConstPtr = std::shared_ptr<const MatrixMultEvaluator>;
 
-  using MatType = Eigen::Matrix<double, DIM, DIM>;
-  using InType = Eigen::Matrix<double, DIM, 1>;
-  using OutType = Eigen::Matrix<double, DIM, 1>;
+  using MatType = Eigen::Matrix<double, ROW, COL>;
+  using InType = Eigen::Matrix<double, COL, 1>;
+  using OutType = Eigen::Matrix<double, ROW, 1>;
 
   static Ptr MakeShared(const typename Evaluable<InType>::ConstPtr& v,
                         const MatType& s);
@@ -38,10 +38,10 @@ class MatrixMultEvaluator : public Evaluable<Eigen::Matrix<double, DIM, 1>> {
 };
 
 // clang-format off
-template <int DIM>
-typename MatrixMultEvaluator<DIM>::Ptr mmult(
-    const typename Evaluable<typename MatrixMultEvaluator<DIM>::InType>::ConstPtr& v,
-    const typename MatrixMultEvaluator<DIM>::MatType& s);
+template <int ROW, int COL = ROW>
+typename MatrixMultEvaluator<ROW, COL>::Ptr mmult(
+    const typename Evaluable<typename MatrixMultEvaluator<ROW, COL>::InType>::ConstPtr& v,
+    const typename MatrixMultEvaluator<ROW, COL>::MatType& s);
 // clang-format on
 
 }  // namespace vspace
@@ -50,34 +50,35 @@ typename MatrixMultEvaluator<DIM>::Ptr mmult(
 namespace steam {
 namespace vspace {
 
-template <int DIM>
-auto MatrixMultEvaluator<DIM>::MakeShared(
+template <int ROW, int COL>
+auto MatrixMultEvaluator<ROW, COL>::MakeShared(
     const typename Evaluable<InType>::ConstPtr& v, const MatType& s) -> Ptr {
   return std::make_shared<MatrixMultEvaluator>(v, s);
 }
 
-template <int DIM>
-MatrixMultEvaluator<DIM>::MatrixMultEvaluator(
+template <int ROW, int COL>
+MatrixMultEvaluator<ROW, COL>::MatrixMultEvaluator(
     const typename Evaluable<InType>::ConstPtr& v, const MatType& s)
     : v_(v), s_(s) {}
 
-template <int DIM>
-bool MatrixMultEvaluator<DIM>::active() const {
+template <int ROW, int COL>
+bool MatrixMultEvaluator<ROW, COL>::active() const {
   return v_->active();
 }
 
-template <int DIM>
-void MatrixMultEvaluator<DIM>::getRelatedVarKeys(KeySet& keys) const {
+template <int ROW, int COL>
+void MatrixMultEvaluator<ROW, COL>::getRelatedVarKeys(KeySet& keys) const {
   v_->getRelatedVarKeys(keys);
 }
 
-template <int DIM>
-auto MatrixMultEvaluator<DIM>::value() const -> OutType {
+template <int ROW, int COL>
+auto MatrixMultEvaluator<ROW, COL>::value() const -> OutType {
   return s_ * v_->value();
 }
 
-template <int DIM>
-auto MatrixMultEvaluator<DIM>::forward() const -> typename Node<OutType>::Ptr {
+template <int ROW, int COL>
+auto MatrixMultEvaluator<ROW, COL>::forward() const ->
+    typename Node<OutType>::Ptr {
   const auto child = v_->forward();
   const auto value = s_ * child->value();
   const auto node = Node<OutType>::MakeShared(value);
@@ -85,10 +86,10 @@ auto MatrixMultEvaluator<DIM>::forward() const -> typename Node<OutType>::Ptr {
   return node;
 }
 
-template <int DIM>
-void MatrixMultEvaluator<DIM>::backward(const Eigen::MatrixXd& lhs,
-                                        const typename Node<OutType>::Ptr& node,
-                                        Jacobians& jacs) const {
+template <int ROW, int COL>
+void MatrixMultEvaluator<ROW, COL>::backward(
+    const Eigen::MatrixXd& lhs, const typename Node<OutType>::Ptr& node,
+    Jacobians& jacs) const {
   const auto child = std::static_pointer_cast<Node<InType>>(node->at(0));
   if (v_->active()) {
     v_->backward(lhs * s_, child, jacs);
@@ -96,11 +97,11 @@ void MatrixMultEvaluator<DIM>::backward(const Eigen::MatrixXd& lhs,
 }
 
 // clang-format off
-template <int DIM>
-typename MatrixMultEvaluator<DIM>::Ptr mmult(
-    const typename Evaluable<typename MatrixMultEvaluator<DIM>::InType>::ConstPtr& v,
-    const typename MatrixMultEvaluator<DIM>::MatType& s) {
-  return MatrixMultEvaluator<DIM>::MakeShared(v, s);
+template <int ROW, int COL>
+typename MatrixMultEvaluator<ROW, COL>::Ptr mmult(
+    const typename Evaluable<typename MatrixMultEvaluator<ROW, COL>::InType>::ConstPtr& v,
+    const typename MatrixMultEvaluator<ROW, COL>::MatType& s) {
+  return MatrixMultEvaluator<ROW, COL>::MakeShared(v, s);
 }
 // clang-format on
 
