fd8d685df3ecb16f2bb37844c413540295866d51
Alec Krawciw <freakyfactoid@gmail.com>
2023-05-08 15:52:09 -0400

    Add LandmarkNoiseEvaluator

--- include/steam/evaluable/stereo/stereo_error_evaluator.hpp
+++ include/steam/evaluable/stereo/stereo_error_evaluator.hpp
@@ -1,7 +1,9 @@
 #pragma once
+#include <iostream>
 
 #include "steam/evaluable/evaluable.hpp"
 #include "steam/evaluable/stereo/compose_landmark_evaluator.hpp"
+#include "steam/evaluable/se3/se3_state_var.hpp"
 
 namespace steam {
 namespace stereo {
@@ -22,6 +24,10 @@ struct CameraIntrinsics {
   /** \brief Focal center offset in the v-coordinate (vertical) */
   double cv;
 };
+
+
+Eigen::Vector4d cameraModel(const CameraIntrinsics::ConstPtr& intrinsics, const Eigen::Vector4d& point);
+Eigen::Matrix4d cameraModelJacobian(const CameraIntrinsics::ConstPtr& intrinsics, const Eigen::Vector4d& point);
 
 /** \brief Stereo camera error function evaluator */
 class StereoErrorEvaluator : public Evaluable<Eigen::Vector4d> {
@@ -51,10 +57,6 @@ class StereoErrorEvaluator : public Evaluable<Eigen::Vector4d> {
                 Jacobians& jacs) const override;
 
  private:
-  Eigen::Vector4d cameraModel(const Eigen::Vector4d& point) const;
-  Eigen::Matrix4d cameraModelJacobian(const Eigen::Vector4d& point) const;
-
- private:
   /** \brief Measurement coordinates extracted from images (ul vl ur vr) */
   const Eigen::Vector4d meas_;
 
@@ -67,5 +69,76 @@ class StereoErrorEvaluator : public Evaluable<Eigen::Vector4d> {
    */
   const ComposeLandmarkEvaluator::ConstPtr eval_;
 };
+
+/** \brief Evaluates the noise of an uncertain map landmark, which has been reprojected into the
+        a query coordinate frame using a steam transform evaluator. */
+class LandmarkNoiseEvaluator : public Evaluable<Eigen::Matrix<double,4,4>> {
+ public:
+
+  /// \brief Constructor
+  /// \param The landmark mean, in the query frame.
+  /// \param The landmark covariance, in the query frame.
+  /// \param The noise on the landmark measurement.
+  /// \param The stereo camera intrinsics.
+  /// \param The steam transform evaluator that takes points from the landmark frame
+  ///        into the query frame.
+  LandmarkNoiseEvaluator(const Eigen::Vector4d& landmark_mean,
+                         const Eigen::Matrix3d& landmark_cov,
+                         const Eigen::Matrix4d& meas_noise,
+                         const CameraIntrinsics::ConstPtr& intrinsics,
+                         const se3::SE3StateVar::ConstPtr& T_query_map);
+
+  /// \brief Default destructor
+  ~LandmarkNoiseEvaluator()=default;
+  
+  /// \brief Evaluates the reprojection covariance 
+  /// @return the 4x4 covariance of the landmark reprojected into the query stereo
+  ///         camera frame.
+  virtual Eigen::Matrix<double,4,4> value();
+
+ private:
+  template <int N>
+  bool positiveDefinite(const Eigen::Matrix<double,N,N> &matrix) {
+  
+    // Initialize an eigen value solver
+    Eigen::SelfAdjointEigenSolver<Eigen::Matrix<double,N,N>> 
+       eigsolver(matrix, Eigen::EigenvaluesOnly);
+
+    // Check the minimum eigen value
+    auto positive_definite = eigsolver.eigenvalues().minCoeff() > 0;
+    if (!positive_definite) {
+      std::cout << "Covariance \n" << matrix << "\n must be positive definite. "
+                << "Min. eigenvalue : " << eigsolver.eigenvalues().minCoeff();
+    }
+    
+    return positive_definite;
+  }
+
+  /// \brief The stereo camera intrinsics.
+  CameraIntrinsics::ConstPtr intrinsics_;
+
+  /// \brief The landmark covariance.
+  Eigen::Matrix4d meas_noise_;
+
+  /// \brief the landmark mean.
+  Eigen::Vector4d mean_;
+
+  /// \brief The steam transform evaluator that takes points from the landmark frame
+  ///        into the query frame.
+  se3::SE3StateVar::ConstPtr T_query_map_;
+
+  /// \brief The 3x3 landmark covariance (phi) dialated into a 3x3 matrix.
+  /// @details dialated_phi_ = D*phi*D^T, where D is a 4x3 dialation matrix.
+  Eigen::Matrix4d dialated_phi_;
+
+  /// \brief The stereo camarea jacobian, evaluated at landmark mean, j.
+  Eigen::Matrix4d camera_jacobian_j_;
+
+  // \brief the last computed covariance
+  Eigen::Matrix<double,4,4> last_computed_cov_;
+};
+
+
+
 }  // end namespace stereo
 }  // namespace steam
--- src/evaluable/stereo/stereo_error_evaluator.cpp
+++ src/evaluable/stereo/stereo_error_evaluator.cpp
@@ -26,13 +26,13 @@ void StereoErrorEvaluator::getRelatedVarKeys(KeySet& keys) const {
 }
 
 auto StereoErrorEvaluator::value() const -> OutType {
-  return meas_ - cameraModel(eval_->value());
+  return meas_ - cameraModel(intrinsics_, eval_->value());
 }
 
 auto StereoErrorEvaluator::forward() const -> Node<OutType>::Ptr {
   // error between measurement and point estimate projected in camera frame
   const auto child = eval_->forward();
-  const auto value = meas_ - cameraModel(child->value());
+  const auto value = meas_ - cameraModel(intrinsics_, child->value());
   const auto node = Node<OutType>::MakeShared(value);
   node->addChild(child);
   return node;
@@ -44,55 +44,124 @@ void StereoErrorEvaluator::backward(const Eigen::MatrixXd& lhs,
   if (eval_->active()) {
     const auto child = std::static_pointer_cast<Node<LmInType>>(node->at(0));
     // Get Jacobians
-    Eigen::Matrix4d new_lhs = (-1) * lhs * cameraModelJacobian(child->value());
+    Eigen::Matrix4d new_lhs = (-1) * lhs * cameraModelJacobian(intrinsics_, child->value());
 
     eval_->backward(new_lhs, child, jacs);
   }
 }
 
-Eigen::Vector4d StereoErrorEvaluator::cameraModel(
-    const Eigen::Vector4d& point) const {
+Eigen::Vector4d cameraModel(
+    const CameraIntrinsics::ConstPtr& intrinsics, const Eigen::Vector4d& point) {
   // Precompute values
   const double x = point[0];
   const double y = point[1];
   const double z = point[2];
   const double w = point[3];
-  const double xr = x - w * intrinsics_->b;
+  const double xr = x - w * intrinsics->b;
   const double one_over_z = 1.0 / z;
 
   // Project point into camera coordinates
   Eigen::Vector4d projectedMeas;
   // clang-format off
-  projectedMeas << intrinsics_->fu * x * one_over_z + intrinsics_->cu,
-                   intrinsics_->fv * y * one_over_z + intrinsics_->cv,
-                   intrinsics_->fu * xr * one_over_z + intrinsics_->cu,
-                   intrinsics_->fv * y * one_over_z + intrinsics_->cv;
+  projectedMeas << intrinsics->fu * x * one_over_z + intrinsics->cu,
+                   intrinsics->fv * y * one_over_z + intrinsics->cv,
+                   intrinsics->fu * xr * one_over_z + intrinsics->cu,
+                   intrinsics->fv * y * one_over_z + intrinsics->cv;
   // clang-format on
   return projectedMeas;
 }
 
-Eigen::Matrix4d StereoErrorEvaluator::cameraModelJacobian(
-    const Eigen::Vector4d& point) const {
+Eigen::Matrix4d cameraModelJacobian(
+    const CameraIntrinsics::ConstPtr& intrinsics, const Eigen::Vector4d& point) {
   // Precompute values
   const double x = point[0];
   const double y = point[1];
   const double z = point[2];
   const double w = point[3];
-  const double xr = x - w * intrinsics_->b;
+  const double xr = x - w * intrinsics->b;
   const double one_over_z = 1.0 / z;
   const double one_over_z2 = one_over_z * one_over_z;
 
   // Construct Jacobian with respect to x, y, z, and scalar w
-  const double dw = -intrinsics_->fu * intrinsics_->b * one_over_z;
+  const double dw = -intrinsics->fu * intrinsics->b * one_over_z;
   Eigen::Matrix4d jac;
   // clang-format off
-  jac << intrinsics_->fu * one_over_z, 0.0, -intrinsics_->fu * x * one_over_z2, 0.0,
-         0.0, intrinsics_->fv * one_over_z, -intrinsics_->fv * y * one_over_z2, 0.0,
-         intrinsics_->fu * one_over_z, 0.0, -intrinsics_->fu * xr * one_over_z2, dw,
-         0.0, intrinsics_->fv * one_over_z, -intrinsics_->fv * y * one_over_z2, 0.0;
+  jac << intrinsics->fu * one_over_z, 0.0, -intrinsics->fu * x * one_over_z2, 0.0,
+         0.0, intrinsics->fv * one_over_z, -intrinsics->fv * y * one_over_z2, 0.0,
+         intrinsics->fu * one_over_z, 0.0, -intrinsics->fu * xr * one_over_z2, dw,
+         0.0, intrinsics->fv * one_over_z, -intrinsics->fv * y * one_over_z2, 0.0;
   // clang-format on
   return jac;
 }
+
+
+LandmarkNoiseEvaluator::LandmarkNoiseEvaluator(const Eigen::Vector4d& landmark_mean,
+                             const Eigen::Matrix3d& landmark_cov,
+                             const Eigen::Matrix4d& meas_noise,
+                             const CameraIntrinsics::ConstPtr& intrinsics,
+                             const se3::SE3StateVar::ConstPtr& T_query_map) : 
+intrinsics_(intrinsics),
+meas_noise_(meas_noise),
+mean_(landmark_mean),
+T_query_map_(T_query_map) {
+  // compute the dialated phi;
+  dialated_phi_.setZero();
+  dialated_phi_.block(0,0,3,3) = landmark_cov;
+  if(!positiveDefinite<3>(landmark_cov)) {
+    std::cout <<  "\nmigrated cov is bad!!!\n";
+  }
+}
+
+// bool LandmarkNoiseEvaluator::active() {
+//   return true;
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief evaluatecovariance
+//////////////////////////////////////////////////////////////////////////////////////////////
+Eigen::Matrix<double,4,4> LandmarkNoiseEvaluator::value() {
+  // TODO: Check to see if we need to recaulculate (add a change flag to steam variables.)
+
+  // Add the measurement noise.
+  last_computed_cov_ = meas_noise_;
+
+  if(!positiveDefinite<4>(meas_noise_)) {
+    std::cout << "measurement noise is bad!!";
+  }
+  // evaluate the steam transform evaluator
+  const lgmath::se3::Transformation T_l_p = T_query_map_->evaluate();
+
+  // Compute the new landmark noise
+  Eigen::Matrix<double,4,4> lm_noise_l = T_l_p.matrix() * dialated_phi_ * T_l_p.matrix().transpose();
+
+  Eigen::Matrix<double,4,3> dialation_matrix;
+  dialation_matrix.setZero();
+  dialation_matrix.block(0,0,3,3) = Eigen::Matrix<double,3,3>::Identity();
+  Eigen::Matrix<double,3,3> lm_noise_l_3 = dialation_matrix.transpose() * lm_noise_l * dialation_matrix;
+
+  if(positiveDefinite<3>(lm_noise_l_3)) {
+    // compute the camera model jacobian based on the transformed mean.
+    camera_jacobian_j_ = stereo::cameraModelJacobian(intrinsics_, T_l_p*mean_);
+
+    Eigen::Matrix<double,4,4> lm_noise = camera_jacobian_j_ * lm_noise_l * camera_jacobian_j_.transpose();
+    Eigen::Matrix<double,3,3> lm_noise_3 = dialation_matrix.transpose() * lm_noise * dialation_matrix;
+
+    if(positiveDefinite<3>(lm_noise_3)) {
+      last_computed_cov_ += lm_noise;
+    } else {
+      std::cout << "\nmigrated noise is not positive definite!!\n";
+    }
+    // return the new noise.
+  } else {
+    std::cout << "\nlm_noise_l is bad!!\n";
+  }
+
+  if (!positiveDefinite<4>(last_computed_cov_)) {
+    std::cout << "sum of noise is bad...";
+  }
+  return last_computed_cov_;
+}
+
 
 }  // end namespace stereo
 }  // namespace steam
