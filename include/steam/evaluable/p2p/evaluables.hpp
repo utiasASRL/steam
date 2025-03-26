f6ce31e422358eb64c23d3ec78096d2b0138099d
Daniil <lisus.daniil@gmail.com>
2025-03-26 14:28:45 -0400

    Removed missed relic of a previous yaw error implementation

--- include/steam/evaluable/p2p/evaluables.hpp
+++ include/steam/evaluable/p2p/evaluables.hpp
@@ -6,6 +6,5 @@
 #include "steam/evaluable/p2p/radial_vel_error_evaluator.hpp"
 #include "steam/evaluable/p2p/p2p_rv_error_evaluator.hpp"
 #include "steam/evaluable/p2p/p2p_error_doppler_evaluator.hpp"
-#include "steam/evaluable/p2p/yaw_error_evaluator.hpp"
 #include "steam/evaluable/p2p/vel_error_evaluator.hpp"
 #include "steam/evaluable/p2p/yaw_vel_error_evaluator.hpp"
